87ad0cf1d06610f312d91f5d56dcd9bb4bf73ad1
Sergio Paucara <sergiopaucara@gmail.com>
2023-03-30 13:31:18 -0400

    Add list user on index

--- workflow/index.php
+++ workflow/index.php
@@ -17,21 +17,94 @@
 	}
 ?>
 
-<form method="POST" action="controlusuario.php">	
-	<div class="wrapper fadeInDown">	
-		<h3>
-			<?php	if ($ok == 1)	echo $textError; ?>
-		</h3>
-		<div id="formContent">
-			<input type="text" id="login" class="fadeIn second" name="usuario" placeholder="Usuario">
-			<input type="password" id="password" class="fadeIn third" name="contrasenia" placeholder="Constrasenia">
-			<input type="submit" class="fadeIn fourth" value="Ingresar">
-			<div id="formFooter">
-				<h2>Sistema</h2>
-			</div>
+
+<div class="container" style="padding: 70px 0px 0px 0px;">
+	<div class="row">
+		<div class="col-8">
+			<form method="POST" action="controlusuario.php">	
+				<div class="wrapper fadeInDown">	
+					<h3>
+						<?php	if ($ok == 1)	echo $textError; ?>
+					</h3>
+					<div id="formContent">
+						<h1>Ingresar</h1>
+						<input type="text" id="login" class="fadeIn second" name="usuario" placeholder="Usuario">
+						<input type="password" id="password" class="fadeIn third" name="contrasenia" placeholder="Constrasenia">
+						<input type="submit" class="fadeIn fourth" value="Ingresar">
+						<div id="formFooter" style="padding: 0px 0px 0px 0px;">
+							<h2 style="margin: 10px 0px 10px 0px;">Sistema</h2>
+						</div>
+					</div>
+				</div>
+			</form>
+		</div>
+		<div class="col-1">
+			<h5>Lista de usuarios:</h5>
+			<table class="table table-sm">
+			<thead>
+				<tr>
+				<th scope="col">#</th>
+				<th scope="col">Rol</th>
+				<th scope="col">User</th>
+				<th scope="col">Pass</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+				<th scope="row">1</th>
+				<td>Universitario</td>
+				<td>sergio</td>
+				<td>123456</td>
+				</tr>
+				<tr>
+					<th scope="row">2</th>
+					<td>Kardex</td>
+					<td>zulema</td>
+					<td>123456</td>
+				</tr>
+				<tr>
+					<th scope="row">3</th>
+					<td>Universitario</td>
+					<td>juan</td>
+					<td>123456</td>
+				</tr>
+				<tr>
+					<th scope="row">4</th>
+					<td>Universitario</td>
+					<td>daniel</td>
+					<td>123456</td>
+				</tr>
+				<tr>
+					<th scope="row">5</th>
+					<td>Kardex</td>
+					<td>virginia</td>
+					<td>123456</td>
+				</tr>
+				<tr>
+					<th scope="row">6</th>
+					<td>Gestiones</td>
+					<td>maria</td>
+					<td>123456</td>
+				</tr>
+				<tr>
+					<th scope="row">7</th>
+					<td>Decano</td>
+					<td>grover</td>
+					<td>123456</td>
+				</tr>
+				<tr>
+					<th scope="row">8</th>
+					<td>Director</td>
+					<td>jose</td>
+					<td>123456</td>
+				</tr>
+			</tbody>
+			</table>
 		</div>
 	</div>
-</form>
+</div>
+
+
 
 <?php 
 	include "pie.inc.php";
